43decd0a348551f723c4e13eaa9a3ad7e8bd4b07
carverh <pcarverb@hotmail.com>
2017-05-15 16:15:31 -0700

    Version 1.1

---
+++ src/index.js
@@ -0,0 +1,42 @@
+var $ = jQuery = require('jquery');
+var bootstrap = require('bootstrap');
+var vis = require('vis');
+var lightbox = require('lightbox2');
+require('./less/main.less');
+
+window.onload = function() {
+  try {
+    var container = document.getElementById('timeline');
+    var data = new vis.DataSet([
+      {id: 1, content: 'Pleistocene', start: '0002'},
+      {id: 2, content: 'Pliocene', start: '0005'},
+      {id: 3, content: 'Miocene', start: '0025'},
+      {id: 4, content: 'Oligocene', start: '0038'},
+      {id: 5, content: 'Eocene', start: '0055'},
+      {id: 6, content: 'Palaeocene', start: '0065'},
+      {id: 7, content: 'Cretaceous', start: '0144'},
+      {id: 8, content: 'Jurassic', start: '0213'},
+      {id: 9, content: 'Triassic', start: '0248'},
+      {id: 10, content: 'Permian', start: '0286'},
+      {id: 11, content: 'Carboniferous', start: '0354'},
+      {id: 12, content: 'Devonian', start: '0412'},
+      {id: 13, content: 'Silurian', start: '0436'},
+      {id: 14, content: 'Ordovician', start: '0352'},
+      {id: 15, content: 'Cambrian', start: '0570'},
+      {id: 16, content: 'Precambrian', start: '0571'},
+    ]);
+    var options = {
+      format: {
+        minorLabels: {
+          year: 'YYYY'
+        },
+        majorLabels: {
+          year: 'YYYY'
+        }
+      }
+    };
+    var timeline = new vis.Timeline(container, data, options);
+  } catch (err) {
+    alert("There was an error :-(\n" + err)
+  }
+}
